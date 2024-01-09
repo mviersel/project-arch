1ea236349584abdabbbb84168154945a02452238
Martijn <119597405+mviersel@users.noreply.github.com>
2024-01-09 22:32:11 +0100

    Poging tot toevoegen van js tot het project

---
+++ popup-menu-bar/twindex/twindex.js
@@ -0,0 +1,10 @@
+const navArrow = document.getElementById("nav-arrow");
+const navItems = document.getElementsByClassName("parent");
+
+const showItems = "nav-items-show";
+
+navArrow.addEventListener("click", function(){
+toggleClass(navItems,)
+})
+
+toggleClass
